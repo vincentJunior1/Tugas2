b636f988693e7d3f61b6a396d90febd91b4d3598
vincentjuniorrr <vincent_cars@yahoo.com>
2020-11-23 10:55:56 +0700

    end 2

--- Tugas2.js
+++ Tugas2.js
@@ -1,29 +1,33 @@
 function searchName(filter,total,callback) {
-    const data = [
-        'Abigail', 'Alexandra', 'Alison',
-        'Amanda', 'Angela', 'Bela', 'Carol',
-        'Caroline', 'Carolyn', 'Deidre',
-        'Diana', 'Elizabeth', 'Ella',
-        'Faith', 'Olivia', 'Penelope'
-    ]
+    if(typeof filter === 'string' && typeof total === 'number'){
 
-    let dataLength = data.length - 1
-
-    let hasil = []
-
-    let angka = 0;
-
-    let newFilter = new RegExp(filter,"gi")
-
-    for (let i = 0; i <= dataLength; i++) {
-        if (data[i].match(newFilter)) {
-            let temp = data[i]
-            data[i] = hasil[angka]
-            hasil[angka] = temp
-            angka++
+        const data = [
+            'Abigail', 'Alexandra', 'Alison',
+            'Amanda', 'Angela', 'Bela', 'Carol',
+            'Caroline', 'Carolyn', 'Deidre',
+            'Diana', 'Elizabeth', 'Ella',
+            'Faith', 'Olivia', 'Penelope'
+        ]
+    
+        let dataLength = data.length - 1
+    
+        let hasil = []
+    
+        let angka = 0;
+    
+        let newFilter = new RegExp(filter,"gi")
+    
+        for (let i = 0; i <= dataLength; i++) {
+            if (data[i].match(newFilter)) {
+                let temp = data[i]
+                hasil[angka] = temp
+                angka++
+            }
         }
+        callback(hasil,total)
+    }else{
+        console.log('Please input with a valid input')
     }
-    callback(hasil,total)
 }
 
 
@@ -32,7 +36,6 @@ function shortName(hasil, total){
     let semuaData = []
     for(let a = 0; a < total; a++){
         let temp = hasil[a]
-        hasil[a] = semuaData[a]
         semuaData[a] = temp
     }
 
--- Tugas3.js
+++ Tugas3.js
@@ -2,22 +2,27 @@ function seleksiNilai (nilaiAwal, nilaiAkhir, dataArray){
     let hasil = []
     let dataLength = dataArray.length -1
     let angka = 0;
-    if(nilaiAwal < nilaiAkhir){
-        for(let i = 0; i <= dataLength; i++ ){
-            if(dataArray[i] >= nilaiAwal && dataArray[i] <= nilaiAkhir){
-                let temp = dataArray[i]
-                dataArray[i] = hasil[angka]
-                hasil[angka] = temp
-                angka++
+    if(dataLength > 5){
+        if(nilaiAwal < nilaiAkhir){
+            for(let i = 0; i <= dataLength; i++ ){
+                if(dataArray[i] >= nilaiAwal && dataArray[i] <= nilaiAkhir){
+                    let temp = dataArray[i]
+                    hasil[angka] = temp
+                    angka++
+                }else{
+                    console.log('Tidak ada data yg sesuai kondisi')
+                }
             }
+        }else{
+            console.log("Nilai Akhir Harus Lebih Besar")
         }
     }else{
-        console.log("Nilai Akhir Harus Lebih Besar")
+        console.log('Array Harus lebih dari 5')
     }
 
-    return console.log([nilaiAwal,nilaiAkhir,hasil])
+    return console.log(hasil)
 }
 
 
-seleksiNilai(5,20,[2,25,4,14,17,30,8])
+seleksiNilai(5,20,[2,25])
 
